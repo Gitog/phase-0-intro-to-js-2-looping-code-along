02da79e472fee737c5c7d87bcad5da1775953f93
James <jamesnjorio@gmail.com>
2022-08-17 11:46:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(gifts, event) {
+
+    const messages = [];
+
+    for (let i = 0; i < gifts.length; i++) {
+
+        const message = (`Thank you, ${gifts[i]}, for the wonderful ${event} gift!`)
+
+        messages.push(message)
+
+        //`Thank you, ${gift}, for the wonderful surprise gift!`
+    }
+    return messages;
+}
+writeCards(gifts, "suprise");
+
+function countDown(n) {
+    while (n > 0) {
+        console.log(n--)
+    }
+    console.log(n);
+}
+countDown(4)
+
+
